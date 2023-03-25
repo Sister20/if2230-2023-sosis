1ccbda5eae5d9c3b79d4541934b1ce1b2ac09266
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-25 11:01:29 +0700

    fix(kernel): testing keyboard driver
    Co-authored-by: Alexander Jason <alexanderjason526@gmail.com>
    Co-authored-by: Eugene Yap <13521074@mahasiswa.itb.ac.id>
    Co-authored-by: Wilson Tansil <13521054@std.stei.itb.ac.id>

--- src/kernel.c
+++ src/kernel.c
@@ -2,16 +2,36 @@
 #include "lib-header/stdtype.h"
 #include "lib-header/stdmem.h"
 #include "lib-header/gdt.h"
+#include "lib-header/idt.h"
+#include "lib-header/interrupt.h"
 #include "lib-header/framebuffer.h"
 #include "lib-header/kernel_loader.h"
+#include "lib-header/keyboard.h"
 
 void kernel_setup(void) {
+    // enter_protected_mode(&_gdt_gdtr);
+    // framebuffer_clear();
+    // framebuffer_write(3, 20,  'H', 0, 0xF);
+    // framebuffer_write(4, 21,  'E', 0, 0xF);
+    // framebuffer_write(3, 22, 'R', 0, 0xF);
+    // framebuffer_write(4, 23, 'E', 0, 0xF);
+    // framebuffer_write(3, 24, '!', 0, 0xF);
+    // framebuffer_write(4, 25, '!', 0, 0xF);
+    // framebuffer_set_cursor(0, 0);
+    // while (TRUE);
     enter_protected_mode(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
     framebuffer_clear();
-    framebuffer_write(3, 8,  'H', 2, 0xF);
-    framebuffer_write(3, 9,  'a', 2, 0xF);
-    framebuffer_write(3, 10, 'i', 2, 0xF);
-    framebuffer_write(3, 11, '!', 2, 0xF);
-    framebuffer_set_cursor(3, 10);
-    while (TRUE);
+    framebuffer_set_cursor(0, 0);
+    activate_keyboard_interrupt();
+    while (TRUE)
+        keyboard_state_activate();
+    // enter_protected_mode(&_gdt_gdtr);
+    // pic_remap();
+    // initialize_idt();
+    // framebuffer_clear();
+    // framebuffer_set_cursor(0, 0);
+    // __asm__("int $0x4");
+    // while (TRUE);
 }
