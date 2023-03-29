79aa7c01bf1acaec5c7b51cb89444caefba071e7
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-30 00:46:12 +0700

    fix(fat32): fix file size count

--- src/kernel.c
+++ src/kernel.c
@@ -36,7 +36,7 @@ void kernel_setup(void)
     struct FAT32DriverRequest request = {
         .buf                   = cbuf,
         .name                  = "ikanaide",
-        .ext                   = "\0\0\0",
+        .ext                   = "uwu",
         .parent_cluster_number = ROOT_CLUSTER_NUMBER,
         .buffer_size           = 0,
     } ;
@@ -45,20 +45,97 @@ void kernel_setup(void)
     memcpy(request.name, "kano1\0\0\0", 8);
     write(request);  // Create folder "kano1"
     memcpy(request.name, "ikanaide", 8);
-    delete(request); // Delete first folder, thus creating hole in FS
+    int8_t ret = delete(request); // Delete first folder, thus creating hole in FS
 
+    if (ret) {}
     memcpy(request.name, "daijoubu", 8);
     request.buffer_size = 5*CLUSTER_SIZE;
     write(request);  // Create fragmented file "daijoubu"
+
+    // struct ClusterBuffer cbuf[5];
+    for (uint32_t i = 0; i < 5; i++)
+        for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
+            cbuf[i].buf[j] = i + 'b';
 
     struct ClusterBuffer readcbuf;
     read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER+1, 1); 
     // If read properly, readcbuf should filled with 'a'
 
+    memcpy(request.name, "newfile\0", 8);
+    request.buf = cbuf;
+    write(request);  // Create fragmented file "newfile"
+
+    memcpy(request.name, "daijoubu", 8);
     request.buffer_size = CLUSTER_SIZE;
-    read(request);   // Failed read due not enough buffer size
+    int8_t read_ret = read(request);   // Failed read due not enough buffer size
+    
     request.buffer_size = 5*CLUSTER_SIZE;
-    read(request);   // Success read on file "daijoubu"
+    read_ret = read(request);   // Success read on file "daijoubu"
+    
+    void *buf = request.buf;
+
+    if (read_ret && buf) {};
+
+    struct ClusterBuffer dbuf;
+    for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
+        dbuf.buf[j] = 'c';
+
+    struct FAT32DriverRequest request2 = {
+        .buf                   = dbuf.buf,
+        .name                  = "request2",
+        .ext                   = "exe",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER + 2,
+        .buffer_size           = 0,
+    } ;
+
+    request2.buffer_size = CLUSTER_SIZE;
+    request2.buf = dbuf.buf;
+    memcpy(request2.name, "filedua\0", 8);
+    int8_t res = write(request2);
+    if (res) {}
+    // enter_protected_mode(&_gdt_gdtr);
+    // pic_remap();
+    // initialize_idt();
+    // activate_keyboard_interrupt();
+    // framebuffer_clear();
+    // framebuffer_set_cursor(0, 0);
+    // initialize_filesystem_fat32();
+    // keyboard_state_activate();
+
+    // struct ClusterBuffer cbuf[5];
+    // for (uint32_t i = 0; i < 5; i++)
+    //     for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
+    //         cbuf[i].buf[j] = i + 'a';
+
+    // struct FAT32DriverRequest request = {
+    //     .buf                   = cbuf,
+    //     .name                  = "ikanaide",
+    //     .ext                   = "uwu",
+    //     .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+    //     .buffer_size           = 0,
+    // } ;
+
+    // write(request);  // Create folder "ikanaide"
+    // memcpy(request.name, "kano1\0\0\0", 8);
+    // write(request);  // Create folder "kano1"
+    // memcpy(request.name, "ikanaide", 8);
+    // memcpy(request.ext, "\0\0\0", 3);
+    // delete(request); // Delete first folder, thus creating hole in FS
+
+    // memcpy(request.name, "daijoubu", 8);
+    // request.buffer_size = 5*CLUSTER_SIZE;
+    // write(request);  // Create fragmented file "daijoubu"
+
+    // struct ClusterBuffer readcbuf;
+    // read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER+1, 1); 
+    // // If read properly, readcbuf should filled with 'a'
+
+    // request.buffer_size = CLUSTER_SIZE;
+    // read(request);   // Failed read due not enough buffer size
+    // request.buffer_size = 5*CLUSTER_SIZE;
+    // read(request);   // Success read on file "daijoubu"
+
+    // while (TRUE);
 
     while (TRUE);
 }
