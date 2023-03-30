66ab67f67ac0c324524d2b30a7ca828d2f19e403
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-30 18:25:49 +0700

    style: clean up code

--- src/idt.c
+++ src/idt.c
@@ -24,13 +24,11 @@ void initialize_idt(void)
 void set_interrupt_gate(uint8_t int_vector, void *handler_address, uint16_t gdt_seg_selector, uint8_t privilege)
 {
     struct IDTGate *idt_int_gate = &interrupt_descriptor_table.table[int_vector];
-    // Set handler offset
     idt_int_gate->offset_low = (uint16_t)((uint32_t)handler_address & 0xFFFF);
     idt_int_gate->offset_high = (uint16_t)(((uint32_t)handler_address >> 16) & 0xFFFF);
-    // Set privilege level
     idt_int_gate->dpl = privilege;
-    // Set segment selector
     idt_int_gate->segment = gdt_seg_selector;
+    
     // Target system 32-bit and flag this as valid interrupt gate
     idt_int_gate->_r_bit_1 = INTERRUPT_GATE_R_BIT_1;
     idt_int_gate->_r_bit_2 = INTERRUPT_GATE_R_BIT_2;
