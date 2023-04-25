3c6c47c94f61899147775a1744795cca7649a3e2
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-25 21:24:47 +0700

    fix: attempt to fix write

--- src/gdt.c
+++ src/gdt.c
@@ -9,84 +9,99 @@
  */
 struct GlobalDescriptorTable global_descriptor_table = {
     .table = {
-        {// Null Descriptor
-         .segment_low = 0,
-         .base_low = 0,
-         .base_mid = 0,
-         .base_high = 0,
-         .type_bit = 0,
-         .non_system = 0,
-         .dpl = 0,
-         .present = 0,
-         .limit = 0,
-         .op_size = 0,
-         .glanularity = 0,
-         },
-        {// Kernel Code
-         .segment_low = 0xFFFF,
-         .base_low = 0,
-         .base_mid = 0,
-         .base_high = 0,
-         .type_bit = 0xA,
-         .non_system = 1,
-         .dpl = 0,
-         .present = 1,
-         .limit = 0xF,
-         .op_size = 1,
-         .glanularity = 1,
-         },
-        {// Kernel Data
-         .segment_low = 0xFFFF,
-         .base_low = 0,
-         .base_mid = 0,
-         .base_high = 0,
-         .type_bit = 0x2,
-         .non_system = 1,
-         .dpl = 0,
-         .present = 1,
-         .limit = 0xF,
-         .op_size = 1,
-         .glanularity = 1,
-         },
-         {// User Code
-         .segment_low = 0xFFFF,
-         .base_low = 0,
-         .base_mid = 0,
-         .base_high = 0,
-         .type_bit = 0xA,
-         .non_system = 1,
-         .dpl = 0x3,
-         .present = 1,
-         .limit = 0xF,
-         .op_size = 1,
-         .glanularity = 1,
-         },
-        {// User Data
-         .segment_low = 0xFFFF,
-         .base_low = 0,
-         .base_mid = 0,
-         .base_high = 0,
-         .type_bit = 0x2,
-         .non_system = 1,
-         .dpl = 0x3,
-         .present = 1,
-         .limit = 0xF,
-         .op_size = 1,
-         .glanularity = 1,
-         },
-         {
-            .limit      = (sizeof(struct TSSEntry) & (0xF << 16)) >> 16,
-            .segment_low       = sizeof(struct TSSEntry),
-            .base_high         = 0,
-            .base_mid          = 0,
-            .base_low          = 0,
-            .non_system        = 0,    // S bit
-            .type_bit          = 0x9,
-            .dpl         = 0,    // DPL
-            .present         = 1,    // P bit
-            .op_size        = 1,    // D/B bit
-            .code_segment         = 0,    // L bit
-            .glanularity       = 0,    // G bit
+        {
+            // Null Descriptor
+            .segment_low = 0,
+            .base_low = 0,
+            .base_mid = 0,
+            .base_high = 0,
+            .type_bit = 0,
+            .non_system = 0,
+            .dpl = 0,
+            .present = 0,
+            .limit = 0,
+            .code_segment = 0,
+            .avl = 0,
+            .op_size = 0,
+            .glanularity = 0,
+        },
+        {
+            // Kernel Code
+            .segment_low = 0xFFFF,
+            .base_low = 0,
+            .base_mid = 0,
+            .base_high = 0,
+            .type_bit = 0xA,
+            .non_system = 1,
+            .dpl = 0,
+            .present = 1,
+            .limit = 0xF,
+            .code_segment = 0,
+            .avl = 0,
+            .op_size = 1,
+            .glanularity = 1,
+        },
+        {
+            // Kernel Data
+            .segment_low = 0xFFFF,
+            .base_low = 0,
+            .base_mid = 0,
+            .base_high = 0,
+            .type_bit = 0x2,
+            .non_system = 1,
+            .dpl = 0,
+            .present = 1,
+            .limit = 0xF,
+            .code_segment = 0,
+            .avl = 0,
+            .op_size = 1,
+            .glanularity = 1,
+        },
+        {
+            // User Code
+            .segment_low = 0xFFFF,
+            .base_low = 0,
+            .base_mid = 0,
+            .base_high = 0,
+            .type_bit = 0xA,
+            .non_system = 1,
+            .dpl = 0x3,
+            .present = 1,
+            .limit = 0xF,
+            .code_segment = 0,
+            .avl = 0,
+            .op_size = 1,
+            .glanularity = 1,
+        },
+        {
+            // User Data
+            .segment_low = 0xFFFF,
+            .base_low = 0,
+            .base_mid = 0,
+            .base_high = 0,
+            .type_bit = 0x2,
+            .non_system = 1,
+            .dpl = 0x3,
+            .present = 1,
+            .limit = 0xF,
+            .code_segment = 0,
+            .avl = 0,
+            .op_size = 1,
+            .glanularity = 1,
+        },
+        {
+            .limit = (sizeof(struct TSSEntry) & (0xF << 16)) >> 16,
+            .segment_low = sizeof(struct TSSEntry),
+            .base_high = 0,
+            .base_mid = 0,
+            .base_low = 0,
+            .non_system = 0, // S bit
+            .type_bit = 0x9,
+            .dpl = 0,          // DPL
+            .present = 1,      // P bit
+            .op_size = 1,      // D/B bit
+            .code_segment = 0, // L bit
+            .glanularity = 0,  // G bit
         },
         {0},
     }};
@@ -101,9 +116,10 @@ struct GDTR _gdt_gdtr = {
     .address = &global_descriptor_table,
 };
 
-void gdt_install_tss(void) {
-    uint32_t base = (uint32_t) &_interrupt_tss_entry;
+void gdt_install_tss(void)
+{
+    uint32_t base = (uint32_t)&_interrupt_tss_entry;
     global_descriptor_table.table[5].base_high = (base & (0xFF << 24)) >> 24;
-    global_descriptor_table.table[5].base_mid  = (base & (0xFF << 16)) >> 16;
-    global_descriptor_table.table[5].base_low  = base & 0xFFFF;
+    global_descriptor_table.table[5].base_mid = (base & (0xFF << 16)) >> 16;
+    global_descriptor_table.table[5].base_low = base & 0xFFFF;
 }
