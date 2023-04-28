94798a52bfe5ff8a37a22b1a2ad12a3616fe3f2f
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-28 08:48:23 +0700

    chore: setup commands.c

---
+++ src/commands.c
@@ -0,0 +1,9 @@
+#include "lib-header/commands.h"
+
+void cd(uint32_t clusterNumber, char* path) {
+
+}
+
+void ls(uint32_t clusterNumber) {
+    
+}
--- src/lib-header/commands.h
+++ src/lib-header/commands.h
@@ -1,5 +1,6 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
+#include "stdtype.h"
 
 void cd(uint32_t clusterNumber, char* path);
 
