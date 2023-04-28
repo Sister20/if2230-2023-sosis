4087af9c8c4ba1b1c7af9c91f60e23083c6da7b0
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-28 21:25:12 +0700

    chore: update fs-syscall signature

--- src/fs-syscall.c
+++ src/fs-syscall.c
@@ -1,29 +1,29 @@
 #include "lib-header/fs-syscall.h"
 
-tssize_t fs_read(struct FAT32DriverRequest *request)
+tssize_t fs_read(struct FAT32DriverRequest request)
 {
     int8_t retcode = 99;
-    syscall(FS_READ, (uint32_t)request, (uint32_t)&retcode, 0);
+    syscall(FS_READ, (uint32_t) &request, (uint32_t)&retcode, 0);
     return (retcode);
 }
 
-tssize_t fs_read_dir(struct FAT32DriverRequest *request)
+tssize_t fs_read_dir(struct FAT32DriverRequest request)
 {
     int8_t retcode = 99;
-    syscall(FS_READ_DIR, (uint32_t)request, (uint32_t)&retcode, 0);
+    syscall(FS_READ_DIR, (uint32_t) &request, (uint32_t)&retcode, 0);
     return (retcode);
 }
 
-tssize_t fs_write(struct FAT32DriverRequest *request)
+tssize_t fs_write(struct FAT32DriverRequest request)
 {
     int8_t retcode = 99;
-    syscall(FS_WRITE, (uint32_t)request, (uint32_t)&retcode, 0);
+    syscall(FS_WRITE, (uint32_t) &request, (uint32_t)&retcode, 0);
     return (retcode);
 }
 
-tssize_t fs_delete(struct FAT32DriverRequest *request)
+tssize_t fs_delete(struct FAT32DriverRequest request)
 {
     int8_t retcode = 99;
-    syscall(FS_DELETE, (uint32_t)request, (uint32_t)&retcode, 0);
+    syscall(FS_DELETE, (uint32_t) &request, (uint32_t)&retcode, 0);
     return (retcode);
 }
--- src/lib-header/fs-syscall.h
+++ src/lib-header/fs-syscall.h
@@ -4,12 +4,12 @@
 #include "syscall.h"
 #include "fat32.h"
 
-tssize_t fs_read(struct FAT32DriverRequest *request);
+tssize_t fs_read(struct FAT32DriverRequest request);
 
-tssize_t fs_read_dir(struct FAT32DriverRequest *request);
+tssize_t fs_read_dir(struct FAT32DriverRequest request);
 
-tssize_t fs_write(struct FAT32DriverRequest *request);
+tssize_t fs_write(struct FAT32DriverRequest request);
 
-tssize_t fs_delete(struct FAT32DriverRequest *request);
+tssize_t fs_delete(struct FAT32DriverRequest request);
 
 #endif
