855a6112f66740d94486d80d88122bff147341a6
Wilson Tansil <13521054@std.stei.itb.ac.id>
2023-04-29 00:19:41 +0700

    feat: add implementation rm and cd (untest)

--- src/commands.c
+++ src/commands.c
@@ -77,3 +77,131 @@ void cat(uint32_t clusterNumber, char* fileName, char* fileExt) {
         }
     }
 }
+
+int cp(uint32_t clusterNumber, char* sourceFileName, char* destinationFileName){
+    struct FAT32DriverRequest dirRequest = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "",
+        .ext                   = "",
+        .parent_cluster_number = clusterNumber,
+        .buffer_size           = 0,
+    };
+
+    tssize_t dirReadResult = fs_read_dir(dirRequest);
+
+    if(dirReadResult != 0){
+        return 1;
+    }
+
+    struct FAT32DirectoryEntry sourceFileEntry = {0};
+    struct FAT32DirectoryTable* dirtable = (struct FAT32DirectoryTable *) dirRequest.buf;
+    int getData = -1;
+    for(tssize_t i = 0; i< CLUSTER_SIZE/sizeof(struct FAT32DirectoryEntry); i++){
+        struct FAT32DirectoryEntry entry = dirtable->table[i];
+        if(entry.name == '\0'){
+            break;
+        }
+        if(entry.attribute != ATTR_SUBDIRECTORY &&
+            memcmp(dirtable->table->name, sourceFileName, 8) == 0){
+            sourceFileEntry = entry;
+            getData = 1;
+            break;
+        }
+    }
+
+    if(getData == -1){
+        return 2;
+    }
+
+    struct FAT32DriverRequest readRequest = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = sourceFileEntry.name,
+        .ext                   = sourceFileEntry.ext,
+        .parent_cluster_number = clusterNumber,
+        .buffer_size           = sourceFileEntry.filesize,
+    };
+
+    tssize_t readResult = fs_read(readRequest);
+    if(readResult != 0){
+        return 3;
+    }
+
+    struct FAT32DriverRequest writeRequest = {
+        .buf                   = readRequest.buf,
+        .name                  = destinationFileName,
+        .ext                   = readRequest.ext,
+        .parent_cluster_number = clusterNumber,
+        .buffer_size           = readRequest.buffer_size,
+    };
+
+    tssize_t writeResult = fs_write(writeRequest);
+
+    if(writeResult != 0){
+        return 4;
+    }
+    return 0;
+}
+
+int rm(uint32_t clusterNumber, char* fileName){
+    struct FAT32DriverRequest dirRequest = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "",
+        .ext                   = "",
+        .parent_cluster_number = clusterNumber,
+        .buffer_size           = 0,
+    };
+
+    tssize_t dirReadResult = fs_read_dir(dirRequest);
+
+    if(dirReadResult != 0){
+        return 1;
+    }
+
+    struct FAT32DirectoryEntry sourceFileEntry = {0};
+    struct FAT32DirectoryTable* dirtable = (struct FAT32DirectoryTable *) dirRequest.buf;
+    int getData = -1;
+    for(tssize_t i = 0; i< CLUSTER_SIZE/sizeof(struct FAT32DirectoryEntry); i++){
+        struct FAT32DirectoryEntry entry = dirtable->table[i];
+        if(entry.name == '\0'){
+            break;
+        }
+        if(entry.attribute != ATTR_SUBDIRECTORY &&
+            memcmp(dirtable->table->name, fileName, 8) == 0){
+            sourceFileEntry = entry;
+            getData = 1;
+            break;
+        }
+    }
+
+    if(getData == -1){
+        return 2;
+    }
+
+    struct FAT32DriverRequest readRequest = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = sourceFileEntry.name,
+        .ext                   = sourceFileEntry.ext,
+        .parent_cluster_number = clusterNumber,
+        .buffer_size           = sourceFileEntry.filesize,
+    };
+
+    tssize_t readResult = fs_read(readRequest);
+    if(readResult != 0){
+        return 3;
+    }
+
+    struct FAT32DriverRequest deleteRequest = {
+        .buf                   = readRequest.buf,
+        .name                  = fileName,
+        .ext                   = readRequest.ext,
+        .parent_cluster_number = clusterNumber,
+        .buffer_size           = readRequest.buffer_size,
+    };
+
+    tssize_t deleteResult = fs_delete(deleteRequest);
+
+    if(deleteResult != 0){
+        return 4;
+    }
+    return 0;
+}
